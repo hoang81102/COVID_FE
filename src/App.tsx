4c9c4bc0972c767fce7633574d721c2ad5ebaa26
REACT10_ThanhHoang <hoangvdtse160556@fpt.edu.vn>
2025-09-18 19:31:28 +0700

    upload1

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Routes, Route} from "react-router-dom";
+import LoginPage from "./page/LoginPage";
+function App() {
+  return (
+   <>
+        <Routes>
+         <Route path="/" element={<LoginPage />} />
+          
+        </Routes>
+
+       <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
+</>
+  );
+}
+
+export default App
